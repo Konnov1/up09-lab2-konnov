bb3c5fd4d36be944502f766c8bded590e79ff7db
ST3512 <ST3512@TE.local>
2024-12-02 14:51:36 +0600

    {posts}

--- labkonnov/lab1/lab1/urls.py
+++ labkonnov/lab1/lab1/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . import hwiews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
+    path('//', hwiews),
 ]
