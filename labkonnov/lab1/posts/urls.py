069027be297f4618a9bd8e764897ba9b4918d98f
unknown <mkonnov155@gmail.com>
2024-12-22 04:01:36 +0600

    postsname

--- labkonnov/lab1/posts/urls.py
+++ labkonnov/lab1/posts/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.posts_list, name="posts"),
+    path('<slug:slug>', views.post_page, name="page"),
+    path('', views.posts_list, name="list")
 ]
